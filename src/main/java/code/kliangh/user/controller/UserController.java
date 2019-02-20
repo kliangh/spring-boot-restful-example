b9bdaaa6007daf82421d31d2e90290d7d364c3ef
kliangh <kunliang.hou@gmail.com>
2019-02-20 23:10:38 +0800

    spring-boot-restful-example. 1. organized imports

--- src/main/java/code/kliangh/user/controller/UserController.java
+++ src/main/java/code/kliangh/user/controller/UserController.java
@@ -2,7 +2,6 @@ package code.kliangh.user.controller;
 
 import code.kliangh.user.entity.User;
 import code.kliangh.user.service.UserService;
-import jdk.nashorn.internal.objects.annotations.Getter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
